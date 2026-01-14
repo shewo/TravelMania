1bfa4c8afc7d748d504f1225cde1022e26421948
Nimsara Lekamge <nimsaralekamge@gmail.com>
2026-01-14 21:49:14 +0530

    full home page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";  
+import Dashboard from "./Main-sections/Dashboard";
+import HomePage from "./Main-sections/Homepage";  
+
+function App() {
+  return (
+    <>
+    <HomePage />  
+  
+    
+   
+  </>
+  
+  );
+}
+
+export default App
