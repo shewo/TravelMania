e0c39a9c25f14fa55ec6cc74e217e5d851ce2fde
sashik mindaka <mindakaofficial0069@gmail.com>
2026-01-16 21:35:12 +0530

    add navbar

--- src/Main-sections/Homepage.jsx
+++ src/Main-sections/Homepage.jsx
@@ -3,6 +3,7 @@ import HomePage1 from '../pages/HomePage1'
 import HomePage2 from '../pages/HomePage2'
 import HomePage3 from '../pages/HomePage3'
 import HomePage4 from '../pages/HomePage4'
+import Navbar from '../components/Navbar'
 function Homepage() {
   return (
     <>
@@ -10,6 +11,7 @@ function Homepage() {
     <HomePage2 /> 
     <HomePage3 />
     <HomePage4 />
+    
 
     </>
   )
