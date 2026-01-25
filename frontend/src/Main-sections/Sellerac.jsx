637bf9e01e6b15ff59b6eed1117e947b1c169ac1
SITHUM MANODHARA <kgsmanodhara@students.nsbm.ac.lk>
2026-01-25 16:35:14 +0530

    yo

---
+++ frontend/src/Main-sections/Sellerac.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Seller from '../pages/Seller.jsx'
+
+
+
+function Sellerac() {
+  return (
+    <>
+   <Seller />
+    
+    
+    </>
+  )
+}
+
+export default Sellerac
