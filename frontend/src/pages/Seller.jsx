2bac50363bfed0438c9bfe5e1e42fbdcb9e6797c
SITHUM MANODHARA <kgsmanodhara@students.nsbm.ac.lk>
2026-01-31 18:49:49 +0530

    yo

--- frontend/src/pages/Seller.jsx
+++ frontend/src/pages/Seller.jsx
@@ -2,6 +2,7 @@ import "../styles/Seller.css";
 import dp from "../assets/dp.jpeg";
 import { MdEmail, MdPhone, MdLocationOn } from "react-icons/md";
 import { FaGlobe } from "react-icons/fa";
+import Sellersidebar from "./Components/Sellersidebar";
 
 
 
@@ -9,21 +10,22 @@ function Seller() {
   return (
     
       <div className="bg-container" >
+        <Sellersidebar />
        <div className="seller"> <h1><b>SELLER DASHBOARD</b></h1></div>
       <div className="overlay-text"><p></p></div>
 
 
 <div className="bio-detail">
   <h3>About us</h3>
-<p>Based in Belihuloya, we provide reliable camping equipment rentals for outdoor enthusiasts. Our services include tents, essential camping gear, and secure vehicle parking, making it easy and safe to enjoy your adventure. We focus on quality equipment, affordable prices, and friendly service to support a comfortable outdoor experience.</p>
+<p></p>
 </div>
 
 <div className="top">
    <div className="seller-info">
-      <h4 className="seller-name">John Perera</h4>
-      <div className="name-line"></div>
-      <p className="seller-role">Camping Equipment Provider</p>
-      <p className="since">Since 2015</p>
+      <h4 className="seller-name"></h4>
+      
+      <p className="seller-role"></p>
+      <p className="since"></p>
     </div>
   <div className="dp-outer">
     <div className="dp-wrapper">
@@ -36,26 +38,13 @@ function Seller() {
 <div className="con">
   <h3 className="contact">Contact Information</h3>
 
-  <div className="contact-list">
-    <div className="contact-item">
-       <MdPhone className="icon" />
-      <span className="label">Phone</span>
-      <span className="value">011 237 5800</span>
-    </div>
 
-    <div className="contact-item">
-      <MdEmail className="icon" />
-      <span className="label">Email</span>
-      <span className="value">info@example.com</span>
-    </div>
-  </div>
 </div>
 
 
-
- 
            
-  </div>    
+  </div>   
+  
 
   )
   }
